054d845bea2a06d6e90fc6c7138a1fececaf5e06
maja <mjpiwinska@gmail.com>
2017-06-09 18:58:08 +0200

    a bit big and messy commit

---
+++ src/main/java/com/example/transformer/service/FriendTransformer.java
@@ -0,0 +1,7 @@
+package com.example.transformer.service;
+
+/**
+ * Created by maja on 31.05.17.
+ */
+public interface FriendTransformer {
+}
---
+++ src/main/java/com/example/transformer/service/impl/FriendTransformerImpl.java
@@ -0,0 +1,7 @@
+package com.example.transformer.service.impl;
+
+/**
+ * Created by maja on 31.05.17.
+ */
+public class FriendTransformerImpl {
+}
